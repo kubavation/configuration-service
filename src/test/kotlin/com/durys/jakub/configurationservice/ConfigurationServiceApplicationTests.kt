83c810314d462c372434b951ae5ada77242d075f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-04 16:20:08 +0100

    init

---
+++ src/test/kotlin/com/durys/jakub/configurationservice/ConfigurationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.configurationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
