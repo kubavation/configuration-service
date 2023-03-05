e030802472f87485893aea5a16bd9ae386063486
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-05 16:42:05 +0100

    init of ModuleConfigurationService

---
+++ src/main/kotlin/com/durys/jakub/configurationservice/service/ModuleConfigurationService.kt
@@ -0,0 +1,8 @@
+package com.durys.jakub.configurationservice.service
+
+import com.durys.jakub.configurationservice.repository.ModuleConfigurationRepository
+import org.springframework.stereotype.Service
+
+@Service
+class ModuleConfigurationService(val repository: ModuleConfigurationRepository) {
+}
