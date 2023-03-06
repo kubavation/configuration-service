8c84151533aaab692a813d21382f7df311631072
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-06 18:28:51 +0100

    ModuleConfigurationService - basic methods

--- src/main/kotlin/com/durys/jakub/configurationservice/repository/ModuleConfigurationRepository.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/repository/ModuleConfigurationRepository.kt
@@ -1,6 +1,11 @@
 package com.durys.jakub.configurationservice.repository
 
+import com.durys.jakub.configurationservice.model.ModuleConfiguration
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 
-interface ModuleConfigurationRepository: MongoRepository<Module, String> {
+interface ModuleConfigurationRepository: MongoRepository<ModuleConfiguration, String> {
+
+    @Query("{'name': ?0}")
+    fun moduleConfiguration(moduleName: String): ModuleConfiguration?
 }
--- src/main/kotlin/com/durys/jakub/configurationservice/service/ModuleConfigurationService.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/service/ModuleConfigurationService.kt
@@ -1,8 +1,18 @@
 package com.durys.jakub.configurationservice.service
 
+import com.durys.jakub.configurationservice.model.ModuleConfiguration
 import com.durys.jakub.configurationservice.repository.ModuleConfigurationRepository
 import org.springframework.stereotype.Service
+import java.lang.RuntimeException
 
 @Service
 class ModuleConfigurationService(val repository: ModuleConfigurationRepository) {
+
+    fun moduleConfiguration(moduleName: String): ModuleConfiguration {
+       return repository.moduleConfiguration(moduleName) ?: throw RuntimeException("Configuration for module $moduleName not found");
+    }
+
+    fun saveModuleConfiguration(moduleConfig: ModuleConfiguration) {
+        repository.save(moduleConfig)
+    }
 }
