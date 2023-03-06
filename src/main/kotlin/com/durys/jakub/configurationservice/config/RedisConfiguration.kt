406ee344c79ed821e7d69e90e3037ce17b403b3a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-06 20:39:53 +0100

    change in protocol version - FIX

--- src/main/kotlin/com/durys/jakub/configurationservice/config/RedisConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/config/RedisConfiguration.kt
@@ -1,8 +1,21 @@
 package com.durys.jakub.configurationservice.config
 
+import io.lettuce.core.ClientOptions
+import io.lettuce.core.protocol.ProtocolVersion
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 
 @Configuration
 @EnableCaching
-class RedisConfiguration
+class RedisConfiguration: LettuceClientConfigurationBuilderCustomizer {
+
+    override fun customize(clientConfigurationBuilder: LettuceClientConfiguration.LettuceClientConfigurationBuilder?) {
+        clientConfigurationBuilder?.clientOptions(ClientOptions.builder()
+                .protocolVersion(ProtocolVersion.RESP2)
+                .build());
+    }
+
+
+}
