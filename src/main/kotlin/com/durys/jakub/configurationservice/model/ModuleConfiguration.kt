9342363b2f85cc315130a3746f94f1d753ce2d47
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-06 19:11:38 +0100

    active status in moduleConfiguration

--- src/main/kotlin/com/durys/jakub/configurationservice/model/ModuleConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/model/ModuleConfiguration.kt
@@ -4,4 +4,5 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("module_configurations")
-data class ModuleConfiguration(@Id val name: String, val desc: String, val configurations: List<Configuration> = emptyList())
+data class ModuleConfiguration(@Id val name: String, val desc: String, val active: Boolean = true,
+                               val configurations: List<Configuration> = emptyList())
