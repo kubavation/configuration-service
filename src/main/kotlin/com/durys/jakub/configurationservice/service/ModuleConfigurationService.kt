52f018f0e2fac99166eac65b47df632b1e808408
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-07 18:21:51 +0100

    unused imports cleaning

--- src/main/kotlin/com/durys/jakub/configurationservice/service/ModuleConfigurationService.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/service/ModuleConfigurationService.kt
@@ -1,10 +1,8 @@
 package com.durys.jakub.configurationservice.service
 
-import com.durys.jakub.configurationservice.model.ModuleConfiguration
 import com.durys.jakub.configurationservice.model.ModuleConfigurationDTO
 import com.durys.jakub.configurationservice.repository.ModuleConfigurationRepository
 import org.springframework.stereotype.Service
-import java.lang.RuntimeException
 
 @Service
 class ModuleConfigurationService(val repository: ModuleConfigurationRepository) {
