d9f3c5b8b41c81096232127edada5dd101ea9cc0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-03-07 18:39:54 +0100

    redis cache serializer configuraution

--- src/main/kotlin/com/durys/jakub/configurationservice/config/RedisConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/config/RedisConfiguration.kt
@@ -4,8 +4,12 @@ import io.lettuce.core.ClientOptions
 import io.lettuce.core.protocol.ProtocolVersion
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair
 
 @Configuration
 @EnableCaching
@@ -17,5 +21,10 @@ class RedisConfiguration: LettuceClientConfigurationBuilderCustomizer {
                 .build());
     }
 
+    @Bean
+    fun cacheConfiguration(): RedisCacheConfiguration {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
+    }
 
 }
