ebb69de219c93f5503e47b466a8cb92e8e37d19b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-18 18:39:43 +0200

    changes in configuration + enabling euerka client

--- src/main/kotlin/com/durys/jakub/configurationservice/ConfigurationServiceApplication.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/ConfigurationServiceApplication.kt
@@ -2,8 +2,10 @@ package com.durys.jakub.configurationservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class ConfigurationServiceApplication
 
 fun main(args: Array<String>) {
