b3fda935dfdc98efaf8675da109747162f167ec0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-20 20:04:14 +0200

    endpoint for getting modules for specific context

--- src/main/kotlin/com/durys/jakub/configurationservice/context/infrastructure/in/ContextController.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/context/infrastructure/in/ContextController.kt
@@ -1,8 +1,10 @@
 package com.durys.jakub.configurationservice.context.infrastructure.`in`
 
+import com.durys.jakub.configurationservice.context.domain.ContextModule
 import com.durys.jakub.configurationservice.context.infrastructure.model.ContextDTO
 import com.durys.jakub.configurationservice.context.infrastructure.ContextRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +17,12 @@ internal class ContextController(val contextRepository: ContextRepository) {
         return contextRepository.findAll().stream()
                 .map { ContextDTO(it.name) }
                 .toList()
+    }
+
+    @GetMapping("/{context}/modules")
+    fun getContextModules(@PathVariable context: String): List<ContextModule> {
+        return contextRepository.findById(context)
+                .map { it.modules }
+                .orElse(listOf())
     }
 }
