60e7858c1a91aa7c13406892d72d40d8e98ba6c1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-21 14:48:04 +0200

    entityNotFoundException class

--- src/main/kotlin/com/durys/jakub/configurationservice/context/infrastructure/in/ContextController.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/context/infrastructure/in/ContextController.kt
@@ -2,14 +2,11 @@ package com.durys.jakub.configurationservice.context.infrastructure.`in`
 
 import com.durys.jakub.configurationservice.context.domain.Context
 import com.durys.jakub.configurationservice.context.domain.ContextModule
-import com.durys.jakub.configurationservice.context.infrastructure.model.ContextDTO
 import com.durys.jakub.configurationservice.context.infrastructure.ContextRepository
+import com.durys.jakub.configurationservice.context.infrastructure.model.ContextDTO
 import com.durys.jakub.configurationservice.context.infrastructure.model.ContextModuleDTO
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.durys.jakub.configurationservice.sharedkernel.exception.EntityNotFoundException
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/contexts")
 @RestController
@@ -33,7 +30,7 @@ internal class ContextController(val contextRepository: ContextRepository) {
     fun setContextModules(@PathVariable context: String, @RequestBody modules: List<ContextModuleDTO>) {
         val entity = contextRepository.findById(context)
                 .map { Context(it.name, modules.map {module -> ContextModule(module.name)}) }
-                .orElseThrow { RuntimeException("not found") }
+                .orElseThrow { EntityNotFoundException(context) }
         contextRepository.save(entity)
     }
 }
