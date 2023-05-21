98cda7dcc884604e0f02c22d9805109ea0255777
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-21 18:06:37 +0200

    ModuleConfigurationAPI

--- src/main/kotlin/com/durys/jakub/configurationservice/api/ModuleConfigurationApi.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/api/ModuleConfigurationApi.kt
@@ -1,22 +1,19 @@
 package com.durys.jakub.configurationservice.api
 
-import com.durys.jakub.configurationservice.moduleconfiguration.infrastructure.model.ModuleConfigurationDTO
 import com.durys.jakub.configurationservice.moduleconfiguration.application.ModuleConfigurationService
-import org.springframework.cache.annotation.CachePut
+import com.durys.jakub.configurationservice.moduleconfiguration.infrastructure.model.ModuleConfigurationDTO
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api/modules")
 @RestController
-internal class ModuleConfigurationController(val moduleConfigurationService: ModuleConfigurationService) {
+internal class ModuleConfigurationApi(val moduleConfigurationService: ModuleConfigurationService) {
 
     @Cacheable(value = ["config"], key = "#moduleName")
-    @GetMapping("/{moduleName}")
+    @GetMapping("/{moduleName}/configuration")
     fun getModuleConfiguration(@PathVariable moduleName: String): ModuleConfigurationDTO {
         return moduleConfigurationService.moduleConfiguration(moduleName)
     }
@@ -24,11 +21,5 @@ internal class ModuleConfigurationController(val moduleConfigurationService: Mod
     @GetMapping("/{moduleName}/configuration/{configName}")
     fun isConfigEnabled(@PathVariable moduleName: String, @PathVariable configName: String): Boolean {
         return moduleConfigurationService.isConfigEnabled(moduleName, configName)
-    }
-
-    @CachePut(value = ["config"], key = "#moduleName")
-    @PostMapping("/{moduleName}")
-    fun setModuleConfiguration(@PathVariable moduleName: String, @RequestBody config: ModuleConfigurationDTO) {
-        moduleConfigurationService.setModuleConfiguration(moduleName, config)
     }
 }
