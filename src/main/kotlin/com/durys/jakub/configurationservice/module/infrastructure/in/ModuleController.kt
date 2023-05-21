908eb5396ad6d1d62d970248592d7c1e901b82d0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-21 17:29:35 +0200

    endpoint for setting ModuleConfigurationPatterns

--- src/main/kotlin/com/durys/jakub/configurationservice/module/infrastructure/in/ModuleController.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/module/infrastructure/in/ModuleController.kt
@@ -1,11 +1,15 @@
 package com.durys.jakub.configurationservice.module.infrastructure.`in`
 
+import com.durys.jakub.configurationservice.module.domain.Module
+import com.durys.jakub.configurationservice.module.domain.ModuleConfigurationPattern
 import com.durys.jakub.configurationservice.module.infrastructure.ModuleRepository
 import com.durys.jakub.configurationservice.module.infrastructure.model.ConfigurationPatternDTO
 import com.durys.jakub.configurationservice.module.infrastructure.model.ModuleDTO
 import com.durys.jakub.configurationservice.sharedkernel.exception.EntityNotFoundException
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,4 +26,17 @@ internal class ModuleController(val moduleRepository: ModuleRepository) {
                 .map { it.configPatterns.map {pattern -> ConfigurationPatternDTO(pattern.name, pattern.description, pattern.defaultValue) } }
                 .orElseThrow { EntityNotFoundException(name) }
     }
+
+    @PatchMapping("/{name}/configuration-pattern")
+    fun setModuleConfigurationPatterns(@PathVariable name: String, @RequestBody configPatterns: List<ConfigurationPatternDTO>) {
+        moduleRepository.findById(name)
+                .map { Module(it.name, it.description, it.configPatterns + to(configPatterns)) }
+                .orElseThrow { EntityNotFoundException(name) }
+    }
+
+    private fun to(configPatterns: List<ConfigurationPatternDTO>): List<ModuleConfigurationPattern> {
+       return configPatterns.map {
+            pattern -> ModuleConfigurationPattern(pattern.name, pattern.description, pattern.defaultValue) }
+    }
+
 }
