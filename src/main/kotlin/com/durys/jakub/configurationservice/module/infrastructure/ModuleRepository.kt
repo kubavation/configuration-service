6ecc46c708eb65cf39a83000cae05663c7965246
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 19:51:27 +0200

    findByName method in moduleRepository

--- src/main/kotlin/com/durys/jakub/configurationservice/module/infrastructure/ModuleRepository.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/module/infrastructure/ModuleRepository.kt
@@ -3,6 +3,9 @@ package com.durys.jakub.configurationservice.module.infrastructure
 import com.durys.jakub.configurationservice.module.domain.Module
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-internal interface ModuleRepository: MongoRepository<Module, String>
+internal interface ModuleRepository: MongoRepository<Module, String> {
+    fun findByName(name: String): Optional<Module>
+}
