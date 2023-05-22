d4f3e249fa0a2bfb74dc8ecd2044173bb347c386
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 17:48:36 +0200

    change in module configuration document  name

--- src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/domain/ModuleConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/domain/ModuleConfiguration.kt
@@ -3,6 +3,6 @@ package com.durys.jakub.configurationservice.moduleconfiguration.domain
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document("module_configurations")
+@Document("module_configuration")
 internal data class ModuleConfiguration(@Id val id: String, val context: String, val module: String,
                                val configurations: List<Configuration> = emptyList())
