649285782fdc50b62626a7efe53ca0834a11c977
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-25 20:17:10 +0200

    endpoint for adding module

--- src/main/kotlin/com/durys/jakub/configurationservice/module/infrastructure/in/ModuleController.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/module/infrastructure/in/ModuleController.kt
@@ -1,5 +1,6 @@
 package com.durys.jakub.configurationservice.module.infrastructure.`in`
 
+import com.durys.jakub.configurationservice.module.domain.Module
 import com.durys.jakub.configurationservice.module.domain.ModuleConfigurationPattern
 import com.durys.jakub.configurationservice.module.infrastructure.ModuleRepository
 import com.durys.jakub.configurationservice.module.infrastructure.model.ConfigurationPatternDTO
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -19,6 +21,10 @@ internal class ModuleController(val moduleRepository: ModuleRepository) {
 
     @GetMapping
     fun getModules(): List<ModuleDTO> = moduleRepository.findAll().map { ModuleDTO(it.name, it.description) }
+
+
+    @PostMapping
+    fun addModule(@RequestBody moduleDTO: ModuleDTO) = moduleRepository.save(Module(moduleDTO.name, moduleDTO.description, emptyList()))
 
     @GetMapping("/{name}/configuration-pattern")
     fun getModuleConfigurationPattern(@PathVariable name: String): List<ConfigurationPatternDTO> {
