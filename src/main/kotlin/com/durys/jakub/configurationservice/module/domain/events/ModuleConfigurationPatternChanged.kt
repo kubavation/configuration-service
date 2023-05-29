9214555e7cc5aa92645885ae9e9e9a430840a68f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-29 17:46:09 +0200

    ModuleConfigurationPatternChanged event

---
+++ src/main/kotlin/com/durys/jakub/configurationservice/module/domain/events/ModuleConfigurationPatternChanged.kt
@@ -0,0 +1,8 @@
+package com.durys.jakub.configurationservice.module.domain.events
+
+import com.durys.jakub.configurationservice.events.DomainEvent
+import com.durys.jakub.configurationservice.module.domain.ModuleConfigurationPattern
+import java.time.LocalDateTime
+import java.util.UUID
+
+internal class ModuleConfigurationPatternChanged(id: UUID, at: LocalDateTime, val patterns: ModuleConfigurationPattern): DomainEvent(id, at)
