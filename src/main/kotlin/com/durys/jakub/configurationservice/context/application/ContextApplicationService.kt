40988e8d88dd6778556fcace0edd8a0f325a1153
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-31 17:50:32 +0200

    publishing ContextModulesChangedEvent in setContextModules method

--- src/main/kotlin/com/durys/jakub/configurationservice/context/application/ContextApplicationService.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/context/application/ContextApplicationService.kt
@@ -2,6 +2,7 @@ package com.durys.jakub.configurationservice.context.application
 
 import com.durys.jakub.configurationservice.context.domain.Context
 import com.durys.jakub.configurationservice.context.domain.ContextModule
+import com.durys.jakub.configurationservice.context.domain.event.ContextModulesChangedEvent
 import com.durys.jakub.configurationservice.context.infrastructure.ContextRepository
 import com.durys.jakub.configurationservice.context.infrastructure.model.ContextModuleDTO
 import com.durys.jakub.configurationservice.events.DomainEventPublisher
@@ -9,13 +10,17 @@ import com.durys.jakub.configurationservice.sharedkernel.exception.EntityNotFoun
 import org.springframework.stereotype.Service
 
 @Service
-internal class ContextApplicationService(val contextRepository: ContextRepository,
-                                         val eventPublisher: DomainEventPublisher)  {
+internal class ContextApplicationService(private val contextRepository: ContextRepository,
+                                         private val eventPublisher: DomainEventPublisher)  {
 
     fun setContextModules(context: String, modules: List<ContextModuleDTO>) {
+
         val entity = contextRepository.findByName(context)
                 .map { Context(it.id, it.name, modules.map {module -> ContextModule(module.name) }) }
+                .map { contextRepository.save(it) }
                 .orElseThrow { EntityNotFoundException(context) }
-        contextRepository.save(entity)
+
+        eventPublisher
+                .publish(ContextModulesChangedEvent(entity.name, entity.modules.map { it.moduleName }))
     }
 }
