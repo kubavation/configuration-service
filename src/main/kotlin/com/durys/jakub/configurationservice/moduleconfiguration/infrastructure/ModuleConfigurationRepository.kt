fe88ae948b6dba9fad1d7b4364aabac4852104bc
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-31 18:14:24 +0200

    additional constructor in module configuration

--- src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/infrastructure/ModuleConfigurationRepository.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/infrastructure/ModuleConfigurationRepository.kt
@@ -3,10 +3,9 @@ package com.durys.jakub.configurationservice.moduleconfiguration.infrastructure
 import com.durys.jakub.configurationservice.moduleconfiguration.domain.ModuleConfiguration
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
+import java.util.*
 
 internal interface ModuleConfigurationRepository: MongoRepository<ModuleConfiguration, String> {
-
-    fun findByModule(moduleName: String): ModuleConfiguration?
 
     @Query("{'context': ?0, 'module': ?1}")
     fun moduleConfiguration(context: String, moduleName: String): ModuleConfiguration?
