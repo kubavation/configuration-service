13a23d16ac9971abbf0b2e6cf4dffc3865c28414
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-01 20:03:07 +0200

    cache evict after ModuleConfigurationPatternChangedEvent

--- src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/application/handler/ModuleConfigurationPatternChangedEventHandler.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/application/handler/ModuleConfigurationPatternChangedEventHandler.kt
@@ -3,6 +3,7 @@ package com.durys.jakub.configurationservice.moduleconfiguration.application.han
 import com.durys.jakub.configurationservice.module.domain.events.ModuleConfigurationPatternChanged
 import com.durys.jakub.configurationservice.moduleconfiguration.infrastructure.ModuleConfigurationRepository
 import mu.KotlinLogging
+import org.springframework.cache.CacheManager
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -10,7 +11,8 @@ import org.springframework.transaction.annotation.Transactional
 private val logger = KotlinLogging.logger {}
 
 @Component
-internal class ModuleConfigurationPatternChangedEventHandler(val moduleConfigurationRepository: ModuleConfigurationRepository) {
+internal class ModuleConfigurationPatternChangedEventHandler(val moduleConfigurationRepository: ModuleConfigurationRepository,
+                                                             val cacheManager: CacheManager) {
 
     @EventListener
     @Transactional
@@ -23,9 +25,13 @@ internal class ModuleConfigurationPatternChangedEventHandler(val moduleConfigura
         val updatedModuleConfigurations = moduleConfigurations.map { it.updateConfigurations(event.patterns)}
 
         moduleConfigurations.map { it.configurations = emptyList(); return@map it; }
-                .forEach { moduleConfigurationRepository.delete(it) }
+                .forEach { moduleConfigurationRepository.delete(it);
+                           cacheManager.getCache("config")?.evict("${it.context},${it.module}")
+                }
 
         moduleConfigurationRepository.saveAll(updatedModuleConfigurations)
+
+
     }
 
 }
