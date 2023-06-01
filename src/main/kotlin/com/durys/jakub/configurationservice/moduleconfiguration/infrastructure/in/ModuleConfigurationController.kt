01513a9db7eb396bcc36e4afaf4b9b0538f02da9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-01 10:29:20 +0200

    fix in cache configuration - ModuleConfigurationController

--- src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/infrastructure/in/ModuleConfigurationController.kt
+++ src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/infrastructure/in/ModuleConfigurationController.kt
@@ -2,6 +2,7 @@ package com.durys.jakub.configurationservice.moduleconfiguration.infrastructure.
 
 import com.durys.jakub.configurationservice.moduleconfiguration.application.ModuleConfigurationService
 import com.durys.jakub.configurationservice.moduleconfiguration.infrastructure.model.ModuleConfigurationDTO
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.web.bind.annotation.*
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.*
 @RestController
 internal class ModuleConfigurationController(val moduleConfigurationService: ModuleConfigurationService) {
 
-    @Cacheable(value = ["config"], key = "#context.#moduleName")
+    @Cacheable(value = ["config"], key = "{#context, #moduleName}")
     @GetMapping("/{context}/{moduleName}")
     fun getModuleConfiguration(@PathVariable context: String, @PathVariable moduleName: String): ModuleConfigurationDTO {
         return moduleConfigurationService.moduleConfiguration(context, moduleName)
     }
 
-    @CachePut(value = ["config"], key = "#context.#moduleName")
+    @CacheEvict(value = ["config"], key = "{#context, #moduleName}")
     @PatchMapping("/{context}/{moduleName}")
     fun setModuleConfiguration(@PathVariable context: String, @PathVariable moduleName: String, @RequestBody config: ModuleConfigurationDTO) {
         moduleConfigurationService.setModuleConfiguration(context, moduleName, config)
