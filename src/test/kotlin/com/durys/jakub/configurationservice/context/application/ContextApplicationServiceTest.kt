f4ba9b8d0429395a3be78c49e85447dadd2a5831
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-04 19:59:35 +0200

    create/edit context - tests

--- src/test/kotlin/com/durys/jakub/configurationservice/context/application/ContextApplicationServiceTest.kt
+++ src/test/kotlin/com/durys/jakub/configurationservice/context/application/ContextApplicationServiceTest.kt
@@ -2,6 +2,7 @@ package com.durys.jakub.configurationservice.context.application
 
 import com.durys.jakub.configurationservice.context.domain.Context
 import com.durys.jakub.configurationservice.context.domain.ContextValidationService
+import com.durys.jakub.configurationservice.context.domain.exception.ContextAlreadyExistsException
 import com.durys.jakub.configurationservice.context.infrastructure.ContextRepository
 import com.durys.jakub.configurationservice.context.infrastructure.model.ContextDTO
 import com.durys.jakub.configurationservice.events.SpringDomainEventPublisher
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.mockito.Mockito
+import java.util.*
 
 internal class ContextApplicationServiceTest {
 
@@ -27,4 +29,39 @@ internal class ContextApplicationServiceTest {
 
         assertDoesNotThrow {contextApplicationService.create(ContextDTO(context.name))}
     }
+
+    @Test
+    fun createContext_shouldThrowException_contextNameAlreadyExists() {
+
+        val context = Context("Warsaw", emptyList())
+
+        Mockito.`when`(contextRepository.save(context)).thenReturn(context)
+        Mockito.`when`(contextValidationService.contextWithNameExists(context.name)).thenReturn(true)
+
+        assertThrows(ContextAlreadyExistsException::class.java) {contextApplicationService.create(ContextDTO(context.name))}
+    }
+
+    @Test
+    fun editContext_shouldSuccessfullySaveContext() {
+
+        val context = Context("Warsaw", emptyList())
+
+        Mockito.`when`(contextRepository.findByName(context.name)).thenReturn(Optional.ofNullable(context))
+        Mockito.`when`(contextRepository.save(context)).thenReturn(context)
+        Mockito.`when`(contextValidationService.contextWithNameExists(context.name)).thenReturn(false)
+
+        assertDoesNotThrow {contextApplicationService.edit(context.name, ContextDTO(context.name))}
+    }
+
+    @Test
+    fun editContext_shouldThrowException_contextNameAlreadyExists() {
+
+        val context = Context("Warsaw", emptyList())
+
+        Mockito.`when`(contextRepository.save(context)).thenReturn(context)
+        Mockito.`when`(contextValidationService.contextWithNameExists(context.name)).thenReturn(true)
+
+        assertThrows(ContextAlreadyExistsException::class.java) {contextApplicationService.edit(context.name, ContextDTO(context.name))}
+    }
+
 }
