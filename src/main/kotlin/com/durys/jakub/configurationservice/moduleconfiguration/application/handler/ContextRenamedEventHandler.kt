f21b0e6db445b2cab36d160ffe74563ffe81ff6a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-05 20:53:26 +0200

    handling ContextRenamedEvent

---
+++ src/main/kotlin/com/durys/jakub/configurationservice/moduleconfiguration/application/handler/ContextRenamedEventHandler.kt
@@ -0,0 +1,21 @@
+package com.durys.jakub.configurationservice.moduleconfiguration.application.handler
+
+import com.durys.jakub.configurationservice.context.domain.event.ContextRenamedEvent
+import com.durys.jakub.configurationservice.moduleconfiguration.domain.ModuleConfiguration
+import com.durys.jakub.configurationservice.moduleconfiguration.infrastructure.ModuleConfigurationRepository
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+internal class ContextRenamedEventHandler(val moduleConfigurationRepository: ModuleConfigurationRepository) {
+
+    @EventListener
+    @Transactional
+    fun handle(event: ContextRenamedEvent) {
+       val configs = moduleConfigurationRepository.contextModuleConfigurations(event.oldContext)
+                .map { ModuleConfiguration(it.id, event.newContext, it.module, it.configurations) }
+        moduleConfigurationRepository.saveAll(configs)
+    }
+
+}
